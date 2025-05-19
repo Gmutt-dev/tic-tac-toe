cea2a1fe950f3728b5165693d7be438197512287
Gmutt-dev <189240431+Gmutt-dev@users.noreply.github.com>
2025-05-19 14:50:34 +0200

    Added a gameboard IIFE with a couple of simple methods
    to have access to a single gameboard object

--- script.js
+++ script.js
@@ -17,10 +17,41 @@ function player(name, marker) {
     return {getName, getMarker};
 }
 
+ // Gameboard factory function as IIFE module
+const gameboard = (function() {
+    const layout = [];
+    
+    // Get current board state
+    function getLayout() {
+        return layout;
+    }
+
+    //create empty gameboard array
+    function resetBoard() {
+        layout.splice(0, layout.length);
+        layout.push(["", "", ""]);
+        layout.push(["", "", ""]);
+        layout.push(["", "", ""]);
+    }
+
+    // Place player marker on the board
+    function placeMarker(marker, row, column) {
+        layout[row][column] = marker;
+    }
+
+    // Create initial empty board state
+    resetBoard();
+
+    return {getLayout, resetBoard, placeMarker};
+})();
+
 ////TEMP console controller for testing
 
 const playerOne = player("John", "X");
 const playerTwo = player("Sarah", "O");
 console.log(playerOne, playerTwo);
+
+gameboard.placeMarker(playerOne.getMarker(), 1, 0);
+console.log(gameboard.getLayout());
 
 ////endTEMP
